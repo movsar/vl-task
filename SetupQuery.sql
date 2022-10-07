9ac81fb4f85d34b848427b6b64507f0c09187a24
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-10-07 21:30:38 +0300

    db script almost finished

--- SetupQuery.sql
+++ SetupQuery.sql
@@ -7,7 +7,6 @@ IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE [name] = N'TestDb')
 CREATE DATABASE TestDb
 GO
 
-
 USE TestDb
 IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE [name] = N'Product')
 CREATE TABLE Product 
@@ -15,16 +14,16 @@ CREATE TABLE Product
 	[ID] uniqueidentifier PRIMARY KEY NOT NULL DEFAULT NewID(),
 	[Name] nvarchar(255) NOT NULL,
 	[Description] nvarchar(max),
-	CONSTRAINT UK_Name UNIQUE(Name)
+
+	CONSTRAINT UK_Product_Name UNIQUE(Name),
+	INDEX IX_Product_Name NONCLUSTERED (Name ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON)
 )
-CREATE NONCLUSTERED INDEX IX_Product_Name ON TestDb.dbo.Product(Name ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON);
-GO
 
 IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE [name] = N'ProductVersion')
 CREATE TABLE ProductVersion 
 (
 	[ID] uniqueidentifier PRIMARY KEY NOT NULL DEFAULT NewID(),
-	[ProductID] uniqueidentifier FOREIGN KEY REFERENCES dbo.Product(ID) NOT NULL,
+	[ProductID] uniqueidentifier FOREIGN KEY REFERENCES dbo.Product(ID) ON DELETE CASCADE NOT NULL,
 	[Name] nvarchar(255) NOT NULL,
 	[Description] nvarchar(max),
 	[CreatedDate] smalldatetime NOT NULL DEFAULT getDate(),
@@ -32,12 +31,103 @@ CREATE TABLE ProductVersion
 	[Height] real NOT NULL,
 	[Length] real NOT NULL,
 
-	CONSTRAINT UK_Name UNIQUE(Name)
+	INDEX IX_ProductVersion_Name NONCLUSTERED (Name ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON),
+	INDEX IX_ProductVersion_CreatedDate NONCLUSTERED (CreatedDate ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON),
+	INDEX IX_ProductVersion_Width NONCLUSTERED (Width ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON),
+	INDEX IX_ProductVersion_Height NONCLUSTERED (Height ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON),
+	INDEX IX_ProductVersion_Length NONCLUSTERED (Length ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON)
 )
+
+IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE [name] = N'EventLog')
+CREATE TABLE EventLog 
+(
+	[ID] uniqueidentifier PRIMARY KEY NOT NULL DEFAULT NewID(),
+	[EventDate] smalldatetime NOT NULL DEFAULT getDate(),
+	[Description] nvarchar(max),
+)
+CREATE NONCLUSTERED INDEX IX_EventLog_EventDate ON TestDb.dbo.EventLog(EventDate ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON);
 GO
 
-CREATE NONCLUSTERED INDEX IX_ProductVersion_Name ON TestDb.dbo.Product(Name ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON);
-CREATE NONCLUSTERED INDEX IX_ProductVersion_CreatedDate ON TestDb.dbo.ProductVersion(CreatedDate ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON);
-CREATE NONCLUSTERED INDEX IX_ProductVersion_Width ON TestDb.dbo.ProductVersion(Width ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON);
-CREATE NONCLUSTERED INDEX IX_ProductVersion_Height ON TestDb.dbo.ProductVersion(Height ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON);
-CREATE NONCLUSTERED INDEX IX_ProductVersion_Length ON TestDb.dbo.ProductVersion(Length ASC) WITH (ALLOW_ROW_LOCKS = ON, ALLOW_ROW_LOCKS = ON);
+CREATE TRIGGER TR_Product_Insert ON Product
+FOR INSERT AS
+BEGIN
+    INSERT INTO EventLog(
+        [EventDate],
+        [Description]
+    )
+    VALUES(
+        getDate(),
+		CONCAT('Inserted ', (SELECT Count(*) FROM Inserted), ' row(s)')
+    );
+END
+GO
+
+CREATE TRIGGER TR_Product_Update ON Product
+FOR UPDATE AS
+BEGIN
+    INSERT INTO EventLog(
+        [EventDate],
+        [Description]
+    )
+    VALUES(
+        getDate(),
+		CONCAT('Updated ', (SELECT Count(*) FROM Inserted), ' row(s)')
+    );
+END
+GO
+
+CREATE TRIGGER TR_Product_Delete ON Product
+FOR DELETE AS
+BEGIN
+    INSERT INTO EventLog(
+        [EventDate],
+        [Description]
+    )
+    VALUES(
+        getDate(),
+		CONCAT('Deleted ', (SELECT Count(*) FROM Deleted), ' row(s)')
+    );
+END
+GO 
+
+
+CREATE TRIGGER TR_ProductVersion_Insert ON ProductVersion
+FOR INSERT AS
+BEGIN
+    INSERT INTO EventLog(
+        [EventDate],
+        [Description]
+    )
+    VALUES(
+        getDate(),
+		CONCAT('Inserted ', (SELECT Count(*) FROM Inserted), ' row(s)')
+    );
+END
+GO
+
+CREATE TRIGGER TR_ProductVersion_Update ON ProductVersion
+FOR UPDATE AS
+BEGIN
+    INSERT INTO EventLog(
+        [EventDate],
+        [Description]
+    )
+    VALUES(
+        getDate(),
+		CONCAT('Updated ', (SELECT Count(*) FROM Inserted), ' row(s)')
+    );
+END
+GO
+
+CREATE TRIGGER TR_ProductVersion_Delete ON ProductVersion
+FOR DELETE AS
+BEGIN
+    INSERT INTO EventLog(
+        [EventDate],
+        [Description]
+    )
+    VALUES(
+        getDate(),
+		CONCAT('Deleted ', (SELECT Count(*) FROM Deleted), ' row(s)')
+    );
+END
