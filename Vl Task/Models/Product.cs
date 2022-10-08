7b204691bfc4773bda9e782bc7d0dceba14a1114
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-10-08 11:15:13 +0300

    added models

---
+++ Vl Task/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vl_Task.Models {
+    public class Product {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        List<ProductVersion> productVersions { get; set; }
+    }
+}
---
+++ Vl Task/Models/ProductVersion.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vl_Task.Models {
+    public class ProductVersion {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        [Required]
+        public int Width { get; set; }
+        [Required]
+        public int Height { get; set; }
+        [Required]
+        public int Length { get; set; }
+    }
+}
